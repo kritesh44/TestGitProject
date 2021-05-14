3ff4d1bf2e49e9081292a257aa1b98a0834fbf06
Ritesh <riteshkumarrajveer@gmail.com>
2021-05-15 03:33:46 +0530

    launch

---
+++ Maven/src/main/java/test/launch.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class launch {
+
+	public static void main(String[] args) {
+		
+		
+		System.setProperty("webdriver.chrome.driver","D:\\Eclipse\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+       
+		driver.get("https://www.google.com");
+		driver.findElement(By.className("gb_g")).click();
+		driver.findElement(By.linkText("Create an account")).click();
+
+	}
+
+}
